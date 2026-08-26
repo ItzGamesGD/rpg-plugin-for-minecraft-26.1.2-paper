38d7c6f682063475b1c86546a61b40d0453fb0a5
ItzGamesGD <ozoz0900upk@gmail.com>
2026-08-26 10:04:46 +0900

    fix(exploration): import primitive port composition

--- src/main/java/com/hyunseo/hyunseorpg/HyunseoRPGPlugin.java
+++ src/main/java/com/hyunseo/hyunseorpg/HyunseoRPGPlugin.java
@@ -168,6 +168,7 @@ import com.hyunseo.hyunseorpg.alchemy.recipe.YamlAlchemyRecipeRegistry;
 import com.hyunseo.hyunseorpg.exploration.ExplorationModule;
 import com.hyunseo.hyunseorpg.exploration.integration.ExistingHyunseoRpgAdapters;
 import com.hyunseo.hyunseorpg.exploration.integration.ExplorationPorts;
+import com.hyunseo.hyunseorpg.exploration.integration.BukkitExplorationPorts;
 import com.hyunseo.hyunseorpg.activity.MiningActivityListener;
 import com.hyunseo.hyunseorpg.activity.MiningActivityService;
 import com.hyunseo.hyunseorpg.crafting.CraftingService;
