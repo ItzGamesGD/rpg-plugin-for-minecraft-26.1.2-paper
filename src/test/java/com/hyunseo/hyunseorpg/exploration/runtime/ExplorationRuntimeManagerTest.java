b1d10a9ec9381d8c2a92d52664b7271554d323b0
ItzGamesGD <ozoz0900upk@gmail.com>
2026-08-27 09:35:09 +0900

    Fix regression test assertion compatibility

--- src/test/java/com/hyunseo/hyunseorpg/exploration/runtime/ExplorationRuntimeManagerTest.java
+++ src/test/java/com/hyunseo/hyunseorpg/exploration/runtime/ExplorationRuntimeManagerTest.java
@@ -154,7 +154,7 @@ final class ExplorationRuntimeManagerTest {
         runtime.markLootTriggerExit(10L);
         assertEquals(10L, runtime.lootTriggerExitAtTick());
         runtime.clearLootTriggerExit();
-        assertNull(runtime.lootTriggerExitAtTick());
+        assertEquals(null, runtime.lootTriggerExitAtTick());
         runtime.markLootTriggerExit(20L);
         runtime.clearCombatAbandonExit();
         assertEquals(20L, runtime.lootTriggerExitAtTick());
